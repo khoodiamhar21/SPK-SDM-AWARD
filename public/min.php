31853800035e595e2c712c885fa1cd281b9d430f
khoodiamhar21 <306242003+khoodiamhar21@users.noreply.github.com>
2026-07-18 15:08:18 +0700

    Hapus file diagnostic & sisa InfinityFree (tidak dipakai Railway)

--- public/min.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-try {
-    require __DIR__ . '/../vendor/autoload.php';
-    echo "AUTOLOAD OK\n";
-} catch (\Throwable $e) {
-    echo "AUTOLOAD ERR: " . $e->getMessage() . "\n";
-    echo $e->getTraceAsString() . "\n";
-}
